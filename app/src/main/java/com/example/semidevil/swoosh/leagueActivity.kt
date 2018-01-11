d995e7b623b843addd8f56bf911f70ac87b94099
Danny Luu <cluu79@gmail.com>
2018-01-10 22:55:56 -0600

    finished skill activity layout

--- app/src/main/java/com/example/semidevil/swoosh/leagueActivity.kt
+++ app/src/main/java/com/example/semidevil/swoosh/leagueActivity.kt
@@ -2,11 +2,19 @@ package com.example.semidevil.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
 
 class leagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View){
+        val skillActivity = Intent( this, SkillActivity::class.java)
+        startActivity(skillActivity)
+
     }
 }
