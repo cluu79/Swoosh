280570fb92ce1e4f636e7924f536c18626c21101
Danny Luu <cluu79@gmail.com>
2018-01-13 09:51:20 -0600

    finished button for skill activity

--- app/src/main/java/com/example/semidevil/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/example/semidevil/swoosh/Controller/FinishActivity.kt
@@ -8,6 +8,6 @@ class FinishActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_finish2)
+        setContentView(R.layout.activity_finish)
     }
 }
--- app/src/main/java/com/example/semidevil/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/semidevil/swoosh/Controller/SkillActivity.kt
@@ -1,9 +1,11 @@
 package com.example.semidevil.swoosh.Controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.semidevil.swoosh.utilities.EXTRA_LEAGUE
 import com.example.semidevil.swoosh.R
+import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : AppCompatActivity() {
     var league = ""
@@ -11,7 +13,15 @@ class SkillActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        finishSkillBtn.setOnClickListener{
+            val skillIntent = Intent(this, FinishActivity::class.java )
+            startActivity(skillIntent)
+        }
+
         league = intent.getStringExtra(EXTRA_LEAGUE)
         println(league)
+
+
     }
 }
